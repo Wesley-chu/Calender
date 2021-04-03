08c81a0471abdf93ed8ae8823085ba4786955974
vvv236 <vvv236@hotmail.com>
2021-04-03 09:10:00 +0900

    add class CalenderDateInfo and put code in ViewController

---
+++ Calender/CalenderDateInfo.swift
@@ -0,0 +1,103 @@
+//
+//  CalenderDateInfo.swift
+//  Calender
+//
+//  Created by 朱偉綸 on 2021/3/29.
+//
+
+import Foundation
+import UIKit
+
+class CalenderDateInfo {
+    var currentYear:Int
+    var currentMonth:Int
+    var colorOfNormalCalender:UIColor
+    var colorOfToday:UIColor
+    
+    
+    var numberOfDaysInThisMonth:Int{
+        let dateComponents = DateComponents(year: currentYear,month: currentMonth)
+        guard let date = Calendar.current.date(from: dateComponents) else { return 0 }
+        let range = Calendar.current.range(of: .day, in: .month, for: date)
+        return range?.count ?? 0
+    }
+
+    var howManyDaysShouldIAddFromFirstFrame:Int{
+        let dateComponents = DateComponents(year: currentYear,month: currentMonth)
+        guard let date = Calendar.current.date(from: dateComponents) else { return 0 }
+        return Calendar.current.component(.weekday, from: date) - 1
+    }
+    
+    var MonthOfLabel:String{
+        return "\(currentYear) 年 \(changeDayForm(day: currentMonth)) 月"
+    }
+    
+    
+    init(currentYear:Int, currentMonth:Int, colorOfNormalCalender:UIColor, colorOfToday:UIColor){
+        self.currentYear = currentYear
+        self.currentMonth = currentMonth
+        self.colorOfNormalCalender = colorOfNormalCalender
+        self.colorOfToday = colorOfToday
+        
+        
+    }
+    
+    func changeDayForm(day:Int) -> String{
+        if day == 1 || day == 2 || day == 3 || day == 4 || day == 5 || day == 6 || day == 7 || day == 8 || day == 9{ return "0\(day)" }else{ return "\(day)" }
+    }
+    
+    func backgroudColor(indexPathRow:Int) -> UIColor{
+        let realNumber = indexPathRow + 1 - self.howManyDaysShouldIAddFromFirstFrame
+        let Year = Calendar.current.component(.year, from: Date())
+        let Month = Calendar.current.component(.month, from: Date())
+        let Day = Calendar.current.component(.day, from: Date())
+        let date = "\(Year)\(changeDayForm(day: Month))\(changeDayForm(day: Day))"
+        
+        if date == "\(currentYear)\(changeDayForm(day: currentMonth))\(changeDayForm(day: realNumber))"{
+            return colorOfToday
+        }else{
+            return colorOfNormalCalender
+        }
+    }
+    
+    func textColor(indexPathRow:Int) -> UIColor{
+        let ch = indexPathRow
+        if ch == 0 || ch == 7 || ch == 14 || ch == 21 || ch == 28 || ch == 35 || ch == 6 || ch == 13 || ch == 20 || ch == 27 || ch == 34 || ch == 41{
+            return UIColor.red
+        }else{
+            return UIColor.black
+        }
+    }
+    
+    func text(indexPathRow:Int) -> String{
+        let realNumber = indexPathRow + 1 - self.howManyDaysShouldIAddFromFirstFrame
+        if indexPathRow < self.howManyDaysShouldIAddFromFirstFrame{
+            return ""
+        }else{
+            return "\(realNumber)"
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Calender/ViewController.swift
+++ Calender/ViewController.swift
@@ -7,11 +7,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var dateCollectionView: UICollectionView!
+    
+    var calenderInfo:CalenderDateInfo?
+    
+    let currentYear = Calendar.current.component(.year, from: Date())
+    let currentMonth = Calendar.current.component(.month, from: Date())
+    let blue = UIColor(red: 176/255, green: 224/255, blue: 255/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        dateCollectionView.delegate = self
+        dateCollectionView.dataSource = self
+        
+        calenderInfo = CalenderDateInfo(currentYear: currentYear, currentMonth: currentMonth, colorOfNormalCalender: blue, colorOfToday: UIColor.brown)
+        setUp()
+        
+    }
+    
+    
+    
+    @IBAction func nextMonth(_ sender: UIButton) {
+        calenderInfo!.currentMonth += 1
+        if calenderInfo!.currentMonth == 13{
+            calenderInfo!.currentMonth = 1
+            calenderInfo!.currentYear += 1
+        }
+        setUp()
+    }
+    
+    
+    @IBAction func previousMonth(_ sender: UIButton) {
+        calenderInfo!.currentMonth -= 1
+        if calenderInfo!.currentMonth == 0{
+            calenderInfo!.currentMonth = 12
+            calenderInfo!.currentYear -= 1
+        }
+        setUp()
+    }
+    
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return calenderInfo!.numberOfDaysInThisMonth + calenderInfo!.howManyDaysShouldIAddFromFirstFrame
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        guard let textLabel = cell.contentView.subviews[0] as? UILabel else { return cell }
+
+        textLabel.text = calenderInfo?.text(indexPathRow: indexPath.row)
+        textLabel.textColor = calenderInfo?.textColor(indexPathRow: indexPath.row)
+        cell.backgroundColor = calenderInfo?.backgroudColor(indexPathRow: indexPath.row)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("chu")
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (dateCollectionView.frame.size.width - 2 * 6) / 7
+        return CGSize(width: width, height: (dateCollectionView.frame.size.height - 10) / 6)
+    }
+    
+    func setUp(){
+        dateLabel.text = calenderInfo!.MonthOfLabel
+        dateCollectionView.reloadData()
     }
 
 
